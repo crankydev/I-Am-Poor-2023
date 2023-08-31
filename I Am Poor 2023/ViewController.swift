b9a77b0520f54ad4b53d67025f1b5304a76f4792
Rachel Woodward <magicalsoprano@gmail.com>
2023-08-31 13:24:22 -0500

    Initial Commit

---
+++ I Am Poor 2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor 2023
+//
+//  Created by Rachel Woodward on 8/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
